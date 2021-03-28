d54a90f62f78752ff612b98de4072fd364a6618a
rami <razmichaelhit@gmail.com>
2021-03-28 22:47:10 +0300

    add face detector

--- ContourswROI.py
+++ ContourswROI.py
@@ -14,15 +14,15 @@ y2 = 0
 
 face_cascade = cv2.CascadeClassifier('Cascade_Detection.xml')
 
+
+
+
 #display width and height
 dispW=640
 dispH=480
 
-
-
-
 #camera settings
-camSet='nvarguscamerasrc !  video/x-raw(memory:NVMM), width=1920, height=1080, format=NV12, framerate=21/1 ! nvvidconv  ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
+camSet='nvarguscamerasrc !  video/x-raw(memory:NVMM), width=3264, height=2464, format=NV12, framerate=21/1 ! nvvidconv  ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
 cam= cv2.VideoCapture(camSet)
 
 #draw ROI - Region Of Interest 
@@ -42,7 +42,7 @@ def mouse_click(event,x,y,flags,params):
 #define servo channels
 kit=ServoKit(channels=16) 
 tilt = 90
-pan = 90
+pan=90
 kit.servo[0].angle=pan
 kit.servo[1].angle=tilt
  
@@ -50,10 +50,22 @@ kit.servo[1].angle=tilt
 def nothing(x):
     pass
 
-
-def create_panTilt_trackbars():
+def create_hsv_trackbars(): 
     cv2.namedWindow('Trackbars')
     cv2.moveWindow('Trackbars',1320,0)
+    
+    cv2.createTrackbar('hueLower', 'Trackbars',46,179,nothing)
+    cv2.createTrackbar('hueUpper', 'Trackbars',86,179,nothing)
+    
+    cv2.createTrackbar('hue2Lower', 'Trackbars',50,179,nothing)
+    cv2.createTrackbar('hue2Upper', 'Trackbars',0,179,nothing)
+    
+    cv2.createTrackbar('satLow', 'Trackbars',157,255,nothing)
+    cv2.createTrackbar('satHigh', 'Trackbars',255,255,nothing)
+    cv2.createTrackbar('valLow','Trackbars',100,255,nothing)
+    cv2.createTrackbar('valHigh','Trackbars',255,255,nothing)
+
+def create_panTilt_trackbars():
     cv2.createTrackbar('Pan', 'Trackbars',90,180,nothing)
     cv2.createTrackbar('Tilt', 'Trackbars',90,180,nothing)
 
@@ -61,6 +73,7 @@ def create_panTilt_trackbars():
 
 
 #create the taskbars
+create_hsv_trackbars()
 create_panTilt_trackbars()
 
 width=cam.get(cv2.CAP_PROP_FRAME_WIDTH)
@@ -143,69 +156,85 @@ tilt=frame1.f_tilt
 kit.servo[0].angle=pan 
 kit.servo[1].angle=tilt
 
-
-cv2.destroyAllWindows()
 while True:   
 
     ret, frame = cam.read()
     #frame=cv2.imread('smarties.png')
-    #hsv=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    hsv=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    hueLow=cv2.getTrackbarPos('hueLower', 'Trackbars')
+    hueUp=cv2.getTrackbarPos('hueUpper', 'Trackbars')
 
+    hue2Low=cv2.getTrackbarPos('hue2Lower', 'Trackbars')
+    hue2Up=cv2.getTrackbarPos('hue2Upper', 'Trackbars')
 
+    Ls=cv2.getTrackbarPos('satLow', 'Trackbars')
+    Us=cv2.getTrackbarPos('satHigh', 'Trackbars')
+
+    Lv=cv2.getTrackbarPos('valLow', 'Trackbars')
+    Uv=cv2.getTrackbarPos('valHigh', 'Trackbars')
+    l_b=np.array([hueLow,Ls,Lv])
+    u_b=np.array([hueUp,Us,Uv])
+ 
+    l_b2=np.array([hue2Low,Ls,Lv])
+    u_b2=np.array([hue2Up,Us,Uv])
+ 
+    FGmask=cv2.inRange(hsv,l_b,u_b)
+    FGmask2=cv2.inRange(hsv,l_b2,u_b2)
+    FGmaskComp=cv2.add(FGmask,FGmask2)
    
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-    global x, y ,w ,h
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-    # Display the output
-
-#Create black white mask for hsv     
-#    cv2.imshow('FGmaskComp',FGmaskComp)
-#    cv2.moveWindow('FGmaskComp',0,530)
-
-    # contours,_=cv2.findContours(FGmaskComp,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-    # contours=sorted(contours,key=lambda x:cv2.contourArea(x),reverse=True)
-    # for cnt in contours:
-    #     area=cv2.contourArea(cnt)
-    #     (x,y,w,h)=cv2.boundingRect(cnt)
-    #     if area>=50:
-
-#      cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),3)
-        objX=x+w    # X position of the right bb of the object we tracking
-        objY=y+h    # Y position of the top of the object we tracking
-        
-        #error between the center of the frame to the object
-        errorPan_right = objX-width/2
-        errorPan_left = x - width/2   
-        errorTilt=objY-height/2 
-        
-        
-        if abs(errorPan_right)>width/2-5:
-            pan=frame2.f_pan
-        if abs(errorPan_left)>width/2-5:
-            pan=frame1.f_pan
-
-        if pan>180:
-            pan=180
-            print("Pan Out of  Range")   
-        if pan<0:
-            pan=0
-            print("Pan Out of  Range") 
-        if tilt>180:
-            tilt=180
-            print("Tilt Out of  Range") 
-        if tilt<0:
-            tilt=0
-            print("Tilt Out of  Range")                 
+   
 
 
-        kit.servo[0].angle=pan
-        kit.servo[1].angle=tilt 
+    cv2.imshow('FGmaskComp',FGmaskComp)
+    cv2.moveWindow('FGmaskComp',0,530)
 
-    cv2.imshow('GrayDetectoion', frame)
-    cv2.moveWindow('GrayDetectoion',530,0)
+    cv2.getTrackbarPos('Pan','Trackbars')
+    cv2.getTrackbarPos('Tilt', 'Trackbars')
 
+    contours,_=cv2.findContours(FGmaskComp,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    contours=sorted(contours,key=lambda x:cv2.contourArea(x),reverse=True)
+    for cnt in contours:
+        print(cnt)
+        area=cv2.contourArea(cnt)
+        (x,y,w,h)=cv2.boundingRect(cnt)
+        if area>=50:
+
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),3)
+            objX=x+w    # X position of the right bb of the object we tracking
+            objY=y+h    # Y position of the top of the object we tracking
+            
+            #error between the center of the frame to the object
+            errorPan_right = objX-width/2
+            errorPan_left = x - width/2   
+            errorTilt=objY-height/2 
+            
+            
+            if abs(errorPan_right)>width/2-5:
+                pan=frame2.f_pan
+            if abs(errorPan_left)>width/2-5:
+                pan=frame1.f_pan
+
+            if pan>180:
+                pan=180
+                print("Pan Out of  Range")   
+            if pan<0:
+                pan=0
+                print("Pan Out of  Range") 
+            if tilt>180:
+                tilt=180
+                print("Tilt Out of  Range") 
+            if tilt<0:
+                tilt=0
+                print("Tilt Out of  Range")                 
+
+
+            kit.servo[0].angle=pan
+            kit.servo[1].angle=tilt 
+            print("HEREEEEEEEEEEEEEEEEEEEEEEE")
+            
+
+    cv2.imshow('nanoCam',frame)
+    cv2.moveWindow('nanoCam',0,0)
     
     if cv2.waitKey(1)==ord('q'):
         break
